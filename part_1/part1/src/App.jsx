74989cf35f2dbae5ef574711e92ae105156eafbc
rajashekarcs2023 <114786116+rajashekarcs2023@users.noreply.github.com>
2024-08-18 21:08:47 -0700

    1.5 done

--- part_1/part1/src/App.jsx
+++ part_1/part1/src/App.jsx
@@ -36,9 +36,9 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts =
-  [
+  const course = {
+    name : 'Half Stack application development',
+    parts : [
     {
       name :'Fundamentals of React',
       exercises : 10
@@ -52,14 +52,15 @@ const App = () => {
       exercises : 14
     }
   ]
+}
   
 
   return (
     <div>
 
-      <Header course={course}/>
-      <Content parts = {parts} />    
-      <Total parts = {parts} />
+      <Header course={course.name}/>
+      <Content parts = {course.parts} />    
+      <Total parts = {course.parts} />
       
       
     </div>
